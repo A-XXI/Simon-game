5fe89fab8232e2708ce7f3d2387d7b916f23d2f3
Arpan <89998736+A-XXI@users.noreply.github.com>
2023-05-18 23:55:48 +0600

    Add files via upload

--- game.js
+++ game.js
@@ -50,7 +50,7 @@ function checkAnswer(currentLevel) {
         $("body").removeClass("game-over");
       }, 200);
 
-      $("#level-title").text("Game Over, Press Any Key to Restart");
+      $("#level-title").text("Game Over, Press Any Key to Restart "+"level was "+ level);
 
       startOver();
     }
